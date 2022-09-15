31845a5d81b77e6f9319388f3c60162a18e69eb1
Priye-Ranjan <anupam.ranjan4u@gmail.com>
2022-09-15 13:41:30 +0530

    user-signin-backend

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,36 @@
 import express from 'express';
+import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
-import cors from 'cors';
+import userRouter from './routes/userRoutes.js';
+
 dotenv.config();
 
-console.log(process.env.MONGODB_URI)
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('connected to db',process.env.MONGODB_URI);
+    console.log('connected to db');
   })
   .catch((err) => {
     console.log(err.message);
   });
 
 const app = express();
-app.use(
-  cors({
-    origin: '*',
-  }),
-);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
 
-const port = process.env.PORT || 5000;
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
